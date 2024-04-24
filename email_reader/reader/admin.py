bbe7f37a9d58de2a3792ebb96a4215c574d3ba92
Данил Кочетов <dmi194@yandex.ru>
2024-04-24 21:13:19 +0300

    Добавил модели, админку, index

--- email_reader/reader/admin.py
+++ email_reader/reader/admin.py
@@ -1,3 +1,49 @@
+from config import config as cf
+from django import forms
 from django.contrib import admin
 
-# Register your models here.
+from .models import Email, EmailAccount
+
+
+class EmailAccountForm(forms.ModelForm):
+    class Meta:
+        model = EmailAccount
+        fields = '__all__'
+        widgets = {
+            'password': forms.PasswordInput(render_value=True),
+        }
+
+
+class EmailAccountAdmin(admin.ModelAdmin):
+    list_display = ('email',)
+    search_fields = ('email',)
+    form = EmailAccountForm
+
+
+class EmailAdmin(admin.ModelAdmin):
+    list_display = (
+        'mailbox', 'short_subject', 'sender', 'recipient',
+        'received', 'display_attachments',
+    )
+    search_fields = ('sender', 'subject', 'received', 'attachments')
+    list_filter = ('mailbox', 'sender', 'recipient', 'received')
+
+    def short_subject(self, obj):
+        if len(obj.subject) == 0:
+            return 'No subject'
+        return (
+            (obj.subject[:cf.ADM_SHORT_SUB] + '...')
+            if len(obj.subject) > cf.ADM_SHORT_SUB
+            else obj.subject
+        )
+    short_subject.short_description = 'Тема'
+
+    def display_attachments(self, obj):
+        if obj.attachments == []:
+            return 'No attachments'
+        return obj.attachments
+    display_attachments.short_description = 'Вложения'
+
+
+admin.site.register(EmailAccount, EmailAccountAdmin)
+admin.site.register(Email, EmailAdmin)
--- email_reader/reader/models.py
+++ email_reader/reader/models.py
@@ -1,3 +1,43 @@
 from django.db import models
+from django_cryptography.fields import encrypt
 
-# Create your models here.
+
+class EmailAccount(models.Model):
+    """
+    Аккаунты электронных почтовых ящиков.
+    """
+    email = models.EmailField(verbose_name='Почтовый аккаунт', unique=True)
+    password = encrypt(models.CharField(verbose_name='Пароль', max_length=128))
+
+    class Meta:
+        verbose_name = 'Account'
+        verbose_name_plural = 'Accounts'
+        ordering = ['email']
+
+    def __str__(self):
+        return self.email
+
+
+class Email(models.Model):
+    """
+    Модель для хранения электронных писем.
+    """
+    message_id = models.CharField(
+        max_length=255, verbose_name='ID письма', unique=True
+    )
+    mailbox = models.EmailField(verbose_name='Почтовый аккаунт')
+    sender = models.EmailField(verbose_name='Отправитель')
+    recipient = models.EmailField(verbose_name='Получатель', blank=True)
+    subject = models.CharField(max_length=255, verbose_name='Тема', blank=True)
+    sent = models.DateTimeField(verbose_name='Время отправки')
+    received = models.DateTimeField(verbose_name='Время получения')
+    text = models.TextField(verbose_name='Текст письма', blank=True)
+    attachments = models.JSONField(verbose_name='Список вложений', blank=True)
+
+    class Meta:
+        verbose_name = 'Email'
+        verbose_name_plural = 'Emails'
+        ordering = ['-received']
+
+    def __str__(self):
+        return self.subject[:20]
